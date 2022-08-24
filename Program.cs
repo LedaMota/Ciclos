12aa77297c643e47e1ac0a930972a3e97479da8b
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-08-24 18:39:11 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ciclo foreak
+            //    string[] nomes = { "Leidiane","duda","joao","Samuel" };
+
+            //    foreach(string nome in nomes)
+            //    {
+            //        Console.WriteLine("nome :" + nome);
+
+            //    }
+
+
+
+
+
+
+
+            //    Console.ReadKey();
+            //}
+
+            #endregion
+        }
+
+
+
+    }
+
+}          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
